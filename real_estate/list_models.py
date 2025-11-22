ba4bf333185125cb9e50fb3a39a123ade9abe7a7
Harisha P C <reach.harishapc@gmail.com>
2025-11-22 23:27:45 +0530

    Delete real_estate directory

--- real_estate/list_models.py
+++
@@ -1,9 +0,0 @@
-import os
-import google.generativeai as genai
-
-os.environ['GOOGLE_API_KEY'] = 'A'
-genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
-
-for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
-        print(m.name)
